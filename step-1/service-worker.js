575d165b2ea2160477dac294339cc9432ef9686e
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2016-11-20 14:55:51 +0530

    Service worker FETCH implemented

--- step-1/service-worker.js
+++ step-1/service-worker.js
@@ -31,8 +31,39 @@ self.addEventListener('activate', function (event) {
 
 });
 
-self.addEventListener('fetch', function (event) {
-  console.log('serviceWorker: fetch event listener')
+self.addEventListener('fetch', function(event) {
+  console.log('[FETCH] - called')
+
+
+	let req = event.request;
+
+
+  event.respondWith(
+    caches.match(req).then(function(res) {
+			if (res) {
+				console.log('[FETCH] - Found in cache');
+				return res;
+			}
+
+      // Else add the request to cache and return the response
+      return fetch(req).then(function(res2) {
+        console.log("[FETCH] - Not found in cache, returning from network");
+
+        // User is Online but wrong URL and page not found
+        if(res2.status === 404) return new Response('[404] - Page not found');
+
+        //Adding to cache
+        var responseToCache = res2.clone();
+        caches.open(cacheName).then(function (cache) {
+          cache.put(req, responseToCache);
+        });
+
+        // Return the response
+        return res;
+
+      });
+    })
+  )
 
 
 });
